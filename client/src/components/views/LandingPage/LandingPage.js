25b3a060754b752152cadfe6c7e592065aae4403
JoeMinKyung <127827627+JoeMinKyung@users.noreply.github.com>
2023-09-28 21:30:55 +0900

    환경 설정

---
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function LandingPage() {
+  return (
+    <div style={{
+        display: 'flex', justifyContent: 'center', alignItems: 'center'
+        , width: '100%', height: '100vh'
+    }}>
+      
+        <h3>yeSQL</h3>
+    </div>
+    
+  )
+}
+
+export default LandingPage
