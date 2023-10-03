da1d66a12fc10abe9de16e301c9a1edfee4fb695
JoeMinKyung <127827627+JoeMinKyung@users.noreply.github.com>
2023-10-03 23:40:05 +0900

    Update: App.js 수정

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,22 @@
-import './App.css';
+import "./App.css";
 import React from "react";
 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link
-} from "react-router-dom";
-
-import LandingPage from './pages/LandingPage';
-import LoginPage from './pages/LoginPage';
-import RegisterPage from './pages/RegisterPage';
+import LandingPage from "./pages/LandingPage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
 
 function App() {
   return (
     <Router>
-      <div>
-        <Routes>
-          <Route exact path="/" element = {<LandingPage />} />
-          <Route path="/login" element = {<LoginPage />} />
-          <Route path="/register" element = {<RegisterPage/>} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route exact path="/" element={<LandingPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Routes>
     </Router>
   );
 }
 
 export default App;
-
