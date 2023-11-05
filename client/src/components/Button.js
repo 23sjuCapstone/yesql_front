cce5f0fe42ac17dd827ae4b41de8af94c1b9ad9e
minkyungJoe1 <mk3701@daum.net>
2023-11-06 07:44:45 +0900

    Fix: yesql-blue 색상 오류로 인해 버튼 색상 변경

--- client/src/components/Button.js
+++ client/src/components/Button.js
@@ -1,14 +1,16 @@
 export const Button = ({
   type = "",
   textColor = "white",
-  buttonColor = "yesql-blue",
+  buttonColor = "blue-600",
   hoverColor = "blue-700",
   textSize = "xl",
-  text = ""
+  text = "",
+  onclick=null
 }) => {
   return (
-    <button
-      class={`type=${type} h-fit text-${textColor} bg-${buttonColor} hover:bg-${hoverColor} text-${textSize} py-2 px-4 rounded-full`}
+    <button type="button"
+      class={`type=${type} h-fit text-${textColor} bg-${buttonColor} hover:bg-${hoverColor} text-${textSize} py-2 px-4 rounded-full`} 
+      onClick ={onclick}
     >
       {text}
     </button>
