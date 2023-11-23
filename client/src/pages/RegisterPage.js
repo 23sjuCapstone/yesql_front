ddbeb4f45dafb0e6bb04861e966890d311351043
minkyungJoe1 <mk3701@daum.net>
2023-11-24 04:01:11 +0900

    CSS: RegisterPage CSS 수정

--- client/src/pages/RegisterPage.js
+++ client/src/pages/RegisterPage.js
@@ -1,51 +1,55 @@
-import React, { useEffect, useState } from "react";
-import { Button } from "../components/Button";
-import img from "../img/visualizationImage.png";
+import React, { useState } from "react";
 import axios from "axios";
+import { useEffect } from "react";
 import { Link, Navigate, useNavigate } from "react-router-dom";
 
-const RegisterPage = () => {
+function LandingPage() {
   const [userId, setUserId] = useState("");
   const [userPassword, setUserPassword] = useState("");
   const [userPasswordCheck, setUserPasswordCheck] = useState("");
+  const [isDubplicated, setisDubplicated] = useState(true);
   const isValid = userId !== "" && isSame === true;
   const navigate = useNavigate();
-  var isDubplicated = false;
 
-  //출력 확인용
   useEffect(() => {
-    console.log("userId=",userId,userPassword, userPasswordCheck);
-  }, [userId, userPassword, userPasswordCheck]);
-  
-   function idDuplicateCheck(){
-     const url = "http://yesql-api.shop:8080";
-     axios
-     .post(
-       url + "/auth/checkDuplicatedId",
-       {},
-       { params: { userId: userId } }
-     )
-     .then((response) => {
-      if(response.data){
-        isDubplicated=true;
-        alert("아이디를 사용할 수 있습니다.");
-      }
-       else{
-        alert("아이디를 사용할 수 없습니다.");
-       }
-     })
-     .catch((error) => {
-       console.error("Error:", error);
-       alert("Request Failed");
-     })
+    console.log("userId=", userId, userPassword, userPasswordCheck);
+    console.log("isdup= ", isDubplicated);
+  }, [userId, userPassword, userPasswordCheck, isDubplicated]);
 
-   }
-   
+  function idDuplicateCheck() {
+    const url = "http://yesql-api.shop:8080";
+    axios
+      .post(url + "/auth/checkDuplicatedId", {}, { params: { userId: userId } })
+      .then((response) => {
+        if (response.data) {
+          if (userId === "") {
+            alert("아이디를 입력해주세요.");
+          } else {
+            setisDubplicated(true);
+            alert("아이디를 사용할 수 있습니다.");
+          }
+        } else {
+          setisDubplicated(false);
+          alert("아이디를 사용할 수 없습니다.");
+        }
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        alert("Request Failed");
+      });
+  }
+
   function auth() {
-    if(userPassword == "" || !isSame() || isDubplicated){
-      alert("아이디나 비밀번호를 확인해주세요.")
-    }
-    else{
+    console.log(isSame(), isDubplicated);
+    if (
+      userId === "" ||
+      userPasswordCheck === "" ||
+      userPassword === "" ||
+      !isSame() ||
+      !isDubplicated
+    ) {
+      alert("아이디나 비밀번호를 확인해주세요.");
+    } else {
       const url = "http://yesql-api.shop:8080";
       axios
         .post(
@@ -61,15 +65,8 @@ const RegisterPage = () => {
         .catch((error) => {
           console.error("Error:", error);
           alert("Request Failed");
-        })
-        // .finally(() => {
-        //   alert("Request Completed");
-        // });
+        });
     }
-    
-  }
-  function message(){
-    alert("클릭");
   }
 
   //비밀번호, 비밀번호 확인 같은지 확인하는 함수
@@ -85,114 +82,113 @@ const RegisterPage = () => {
   const goToLogin = () => {
     navigate("/login");
   };
-  const goToLanding = () =>{
+  const goToLanding = () => {
     navigate("/");
-  }
-
-  // //에러메세지용 padding 관리
-  // const handlePasswordChange = () => {
-  //   if (userPasswordCheck !== "" && !isSame()) {
-  //     setjoininPaddingsize(24);
-  //   } else {
-  //     setjoininPaddingsize(28);
-  //   }
-  // };
-
-  // //useEffect로 실시간 출력
-  // useEffect(() => {
-  //   handlePasswordChange();
-  // }, [userPasswordCheck, isSame]);
+  };
 
   return (
-    <div className="RegisterPage relative h-screen">
-      <li className="text-6xl text-yesql-blue pb-12 list-none">
-        <Link to="/">yeSQL</Link>
-      </li>
-      <form>
-        <div className="flex gap-4">
-          <div className="register-box flex flex-col border border-black w-80 rounded-xl">
-            <input type="text" placeholder="id" class="py-2.5 pl-6 bg-transparent rounded-md shadow-sm 
-            focus:outline-none focus:border-sky-500 focus:ring focus:ring-sky-500
-            disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
-            invalid:border-pink-500 invalid:text-pink-600
-            focus:invalid:border-pink-500 focus:invalid:ring-pink-500
-            " value={userId} onChange={(e) => {
-              console.log(e.target.value);
-              setUserId(e.target.value);
-            }}>
-              
-            </input>
-            
-            <Input
-              classStyle="border-t border-b border-black"
-              type="password"
-              name="password"
-              placeholder="password"
-              maxlength={20}
-              onChange={setUserPassword}
-            />
-            <Input
-              type="password"
-              name="password-check"
-              placeholder="password check"
-              maxlength={20}
-              onChange={setUserPasswordCheck}
-            />
+    <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+      <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+        {/* <img class="mx-auto h-10 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company"> */}
+        <li className="my-10 text-center text-6xl font-bold leading-9 tracking-tight text-yesql list-none">
+          <Link to="/">yeSQL</Link>
+        </li>
+      </div>
+
+      <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form class="space-y-6">
+          <div>
+            {/* <div className="relative"> */}
+            <label
+              for="email"
+              class="text-sm font-semibold leading-6 text-gray-900"
+            >
+              User ID
+            </label>
+            <button
+              class="float-right rounded-3xl bg-yesql px-3 py-1 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              onClick={() => idDuplicateCheck()}
+              type="button"
+            >
+              id check
+            </button>
+            {/* </div> */}
+
+            <div class="mt-2">
+              <input
+                className={`${
+                  !isDubplicated
+                    ? "border-2 border-red-500 focus:outline-none"
+                    : "focus:outline-none focus:ring-2 focus:ring-inset focus:ring-yesql"
+                } block w-full rounded-md border-0 px-1.5 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-sm sm:leading-6`}
+                type="text"
+                value={userId}
+                onChange={(e) => {
+                  setUserId(e.target.value);
+                }}
+              />
+            </div>
           </div>
 
-          <Button
-          onclick={idDuplicateCheck}
-            type="idcheck"
-            textColor="black"
-            buttonColor="yesql-blue/50"
-            hoverColor="blue-700"
-            text="id check"
-          />
-        </div>
+          <div>
+            <div class="flex items-center justify-between">
+              <label
+                for="password-check"
+                class="block text-sm font-semibold leading-6 text-gray-900"
+              >
+                Password Check
+              </label>
+            </div>
+            <div class="mt-2">
+              <input
+                id="password-check"
+                name="password-check"
+                type="password"
+                class="block w-full rounded-md border-0 px-1.5 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-yesql sm:text-sm sm:leading-6 required:border-red-500 invalid:border-red-500"
+                value={userPasswordCheck}
+                onChange={(e) => {
+                  setUserPasswordCheck(e.target.value);
+                }}
+              />
+            </div>
+          </div>
 
-        <div
-          className="mt-2"
-          const disabled={isValid ? false : true}
-          //onClick={() => goToLogin()}
-        >
-          {userPasswordCheck !== "" && !isSame() && (
-            <p className="passwordCheck text-sm text-red-600 font-thin">
-              비밀번호가 일치하지 않습니다.
-            </p>
-          )}
-          <Button onclick={auth} type="joinin" text="join in" />
-        </div>
-      </form>
-      <div class="absolute bottom-0 right-0">
-        <div className="img opacity-50">
-          <img src={img} />
-        </div>
+          <div>
+            <div class="flex items-center justify-between">
+              <label
+                for="password"
+                class="block text-sm font-semibold leading-6 text-gray-900"
+              >
+                Password
+              </label>
+            </div>
+            <div class="mt-2">
+              <input
+                id="password"
+                name="password"
+                type="password"
+                class="block w-full rounded-md border-0 px-1.5 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-yesql sm:text-sm sm:leading-6 required:border-red-500 invalid:border-red-500"
+                value={userPassword}
+                onChange={(e) => {
+                  setUserPassword(e.target.value);
+                }}
+              />
+            </div>
+          </div>
+
+          <div>
+            <button
+              type="button"
+              class="flex w-full justify-center rounded-md bg-yesql px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              onClick={() => auth()}
+            >
+              Join in
+            </button>
+          </div>
+        </form>
       </div>
     </div>
   );
-};
+}
 
-const Input = ({
-  classStyle = "",
-  type = "",
-  name = "",
-  placeholder = "",
-  maxlength = 0,
-  onChange = null
-}) => {
-  return (
-    <input
-      className={`py-2.5 pl-6 bg-transparent ${classStyle}`}
-      type={type}
-      name={name}
-      placeholder={placeholder}
-      size="20"
-      maxLength={maxlength}
-      onChange={(e) => {
-        onChange(e.target.value);
-      }}
-    ></input>
-  );
-};
-
-export default RegisterPage;
+export default LandingPage;
