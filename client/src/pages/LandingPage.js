0ba4d5869ee37bfbf0e6a7516671361fac8a8f9e
minkyungJoe1 <mk3701@daum.net>
2023-11-24 04:00:31 +0900

    CSS: LandingPage CSS 수정

--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -5,45 +5,63 @@ import { Link } from "react-router-dom";
 
 function LandingPage() {
   return (
-    <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
-  <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-    {/* <img class="mx-auto h-10 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company"> */}
-    <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign in to your account</h2>
-  </div>
-
-  <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-    <form class="space-y-6" action="#" method="POST">
-      <div>
-        <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
-        <div class="mt-2">
-          <input id="email" name="email" type="email" autocomplete="email" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+    <div className="flex h-full flex-col">
+      <h1 className="py-10">
+        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+          <nav className="relative z-50 flex justify-between">
+            <div className="flex items-center md:gap-x-12 font-black text-yesql text-xl">
+              yeSQL
+            </div>
+          </nav>
         </div>
-      </div>
-
-      <div>
-        <div class="flex items-center justify-between">
-          <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
-          <div class="text-sm">
-            <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500">Forgot password?</a>
+      </h1>
+      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 text-center mt-10">
+        <h1 className="mx-auto max-w-4xl font-display text-5xl font-semibold tracking-tight text-slate-900 sm:text-7xl">
+          Experience
+          <div className="relative whitesoace-nowrap">
+            <div className="relative">
+              the power of
+              <span className="text-yesql"> visualization</span>
+            </div>
           </div>
-        </div>
-        <div class="mt-2">
-          <input id="password" name="password" type="password" autocomplete="current-password" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+          {/* <span className="relative whitesoace-nowrap">
+          <span className="relative"> the power of visualization </span>
+        </span> */}
+          and learn SQL easily
+        </h1>
+        <p className="mx-auto mt-14 max-w-2xl text-lg tracking-tight text-slate-700">
+          Our platform simplifies the learning process by visually representing
+          the query execution steps. Start your SQL journey today and master
+          this powerful language with ease
+        </p>
+        <div className="mt-16 flex justify-center gap-x-10 lg:mt-4">
+          <a
+            href="/register"
+            className="group flex inline-flex bg-yesql h-9 w-full items-center justify-center py-2 px-4 overflow-hidden rounded-full transition-all duration-300 text-white text-lg font-semibold hover:scale-110 active:duration-75 active:scale-95 sm:w-max focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2"
+            //text-white hover:bg-slate-700 hover:text-slate-100 active:bg-slate-800 active:text-slate-300 focus-visible:outline-slate-900
+          >
+            Get Started
+          </a>
+          <a
+            href="/login"
+            className="group flex inline-flex bg-yesql h-9 w-full items-center justify-center py-2 px-4 overflow-hidden rounded-full transition-all duration-300 text-white text-lg font-semibold hover:scale-110 active:duration-75 active:scale-95 sm:w-max focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2"
+          >
+            Sing in
+          </a>
         </div>
       </div>
-
-      <div>
-        <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
-      </div>
-    </form>
-
-    <p class="mt-10 text-center text-sm text-gray-500">
-      Not a member?
-      <a href="#" class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Start a 14 day free trial</a>
-    </p>
-  </div>
-</div>
-    );
+      <p class="mt-4 text-center text-sm text-gray-500">
+        New to yeSQL?
+        <a
+          href="#"
+          class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+        >
+          {" "}
+          There is a tutorial!
+        </a>
+      </p>
+    </div>
+  );
 }
 
 export default LandingPage;
