afb369bcdb9253fe220e2258ea7bd589f8abc83a
JoeMinKyung <127827627+JoeMinKyung@users.noreply.github.com>
2023-11-24 21:18:13 +0900

    Feat: 버튼 컴포넌트 기능 수정

--- client/src/components/Button.js
+++ client/src/components/Button.js
@@ -1,6 +1,20 @@
-export const Button = ({textColor='white', buttonColor='yesql-blue',hoverColor='blue-700', textSize='xl', text=''}) => {
-    return (
-        <button class={`h-fit text-${textColor} bg-${buttonColor} hover:bg-${hoverColor} text-${textSize} py-2 px-4 rounded-full`}>{text}</button>
-    )
-}
-
+export const Button = ({
+  type = "",
+  textColor = "white",
+  buttonColor = "yesql",
+  hoverColor = "blue-700",
+  textSize = "xl",
+  text = "",
+  onclick = null,
+  buttontype = "button"
+}) => {
+  return (
+    <button
+      type={buttontype}
+      class={`type=${type} h-fit text-${textColor} bg-${buttonColor} hover:bg-${hoverColor} text-${textSize} py-2 px-4 rounded-full`}
+      onClick={onclick}
+    >
+      {text}
+    </button>
+  );
+};
