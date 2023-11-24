5770e8bfc3cdb871d387bf2595caf56df79f48e5
JoeMinKyung <127827627+JoeMinKyung@users.noreply.github.com>
2023-11-24 21:18:31 +0900

    CSS: LandingPage 수정

--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -2,33 +2,64 @@ import React from "react";
 import img from "../img/LandingPageBackgroundLeft.png";
 import img2 from "../img/LandingPageBackgroundRight.png";
 import { Link } from "react-router-dom";
+
 function LandingPage() {
   return (
-    <div className="LandingPage">
-      <div class="relative h-screen">
-        <div class="absolute bottom-0 left-0 z-0">
-          <img src={img} />
+    <div className="flex h-full flex-col">
+      <h1 className="py-10">
+        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+          <nav className="relative z-50 flex justify-between">
+            <div className="flex items-center md:gap-x-12 font-black text-yesql text-xl">
+              yeSQL
+            </div>
+          </nav>
         </div>
-        <div class="absolute inset-y-0 right-0 z-10">
-          <img src={img2} />
-        </div>
-        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-          <p className="text-9xl text-yesql-blue z-20 mb-20 text-center">
-            yeSQL
-          </p>
-          <ul className="flex flex-row space-x-32">
-            <li className="text-2xl">
-              <Link to="/">튜토리얼</Link>
-            </li>
-            <li className="text-2xl">
-              <Link to="/login">로그인</Link>
-            </li>
-            <li className="text-2xl">
-              <Link to="/register">회원가입</Link>
-            </li>
-          </ul>
+      </h1>
+      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 text-center mt-10">
+        <h1 className="mx-auto max-w-4xl font-display text-5xl font-semibold tracking-tight text-slate-900 sm:text-7xl">
+          Experience
+          <div className="relative whitesoace-nowrap">
+            <div className="relative">
+              the power of
+              <span className="text-yesql"> visualization</span>
+            </div>
+          </div>
+          {/* <span className="relative whitesoace-nowrap">
+          <span className="relative"> the power of visualization </span>
+        </span> */}
+          and learn SQL easily
+        </h1>
+        <p className="mx-auto mt-14 max-w-2xl text-lg tracking-tight text-slate-700">
+          Our platform simplifies the learning process by visually representing
+          the query execution steps. Start your SQL journey today and master
+          this powerful language with ease
+        </p>
+        <div className="mt-16 flex justify-center gap-x-10 lg:mt-4">
+          <a
+            href="/register"
+            className="group flex inline-flex bg-yesql h-9 w-full items-center justify-center py-2 px-4 overflow-hidden rounded-full transition-all duration-300 text-white text-lg font-semibold hover:scale-110 active:duration-75 active:scale-95 sm:w-max focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2"
+            //text-white hover:bg-slate-700 hover:text-slate-100 active:bg-slate-800 active:text-slate-300 focus-visible:outline-slate-900
+          >
+            Get Started
+          </a>
+          <a
+            href="/login"
+            className="group flex inline-flex bg-yesql h-9 w-full items-center justify-center py-2 px-4 overflow-hidden rounded-full transition-all duration-300 text-white text-lg font-semibold hover:scale-110 active:duration-75 active:scale-95 sm:w-max focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2"
+          >
+            Sing in
+          </a>
         </div>
       </div>
+      <p class="mt-4 text-center text-sm text-gray-500">
+        New to yeSQL?
+        <a
+          href="#"
+          class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+        >
+          {" "}
+          There is a tutorial!
+        </a>
+      </p>
     </div>
   );
 }
