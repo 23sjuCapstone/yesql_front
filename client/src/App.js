cfaed66b1b408554992c76e43f67d29048078df1
JoeMinKyung <127827627+JoeMinKyung@users.noreply.github.com>
2023-11-24 21:17:54 +0900

    no message

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
+import VisualizationPage from "./pages/VisualizationPage";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route exact path="/" element={<LandingPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
+        <Route path="/visual" element={<VisualizationPage />} />
       </Routes>
     </Router>
   );
