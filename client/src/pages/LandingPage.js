a15a0b1a9e8294cd2d45cd68aab6b4c7f025bacc
sungyeon <csy357@gmail.com>
2023-11-30 05:20:10 +0900

    Style : 랜딩페이지 스타일 수정

--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -2,6 +2,7 @@ import React from "react";
 import logo from "../img/logo.png";
 import { Link } from "react-router-dom";
 import "./LandingPage.css";
+import { size } from "lodash";
 
 function LandingPage() {
   return (
@@ -10,7 +11,7 @@ function LandingPage() {
         <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
           <nav className="relative z-50 flex justify-between">
             <div className="flex items-center md:gap-x-12 font-black text-yesql text-xl">
-             <img src={logo} width='121.35' height='15'/>
+              <img src={logo} width="121.35" height="15" />
             </div>
           </nav>
         </div>
@@ -18,40 +19,47 @@ function LandingPage() {
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 text-center mt-10">
         <h1 className="mx-auto max-w-4xl font-display text-5xl font-semibold tracking-tight text-slate-900 sm:text-7xl">
           <span className="giveshadow">눈</span>으로 하는 편한
-          <div className="relative whitesoace-nowrap">
+          <div className="relative whitesoace-nowrap m-2">
             <div className="relative">
               <span className="giveshadow">SQL 공부</span>
             </div>
           </div>
-          경험해보세요
+          <div className="mt-4">경험해보세요</div>
         </h1>
-        <p className="mx-auto mt-14 max-w-2xl text-lg tracking-tight text-slate-700">
-
-        </p>
+        <p className="mx-auto mt-14 max-w-2xl text-lg tracking-tight text-slate-700"></p>
         <div className="mt-16 flex justify-center gap-x-10 lg:mt-4">
           <a
             href="/register"
-            className="group flex inline-flex bg-yesql h-9 w-full items-center justify-center py-2 px-4 overflow-hidden rounded-full transition-all duration-300 text-white text-lg font-semibold hover:scale-110 active:duration-75 active:scale-95 sm:w-max focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2"
+            className="button group flex inline-flex bg-yesql h-9 w-full items-center justify-center py-2 px-4 overflow-hidden rounded-full transition-all duration-300 text-white text-lg font-semibold hover:scale-110 active:duration-75 active:scale-95 sm:w-max focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2"
           >
             시작하기
           </a>
           <a
             href="/login"
-            className="group flex inline-flex bg-yesql h-9 w-full items-center justify-center py-2 px-4 overflow-hidden rounded-full transition-all duration-300 text-white text-lg font-semibold hover:scale-110 active:duration-75 active:scale-95 sm:w-max focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2"
+            className="button group flex inline-flex bg-yesql h-9 w-full items-center justify-center py-2 px-4 overflow-hidden rounded-full transition-all duration-300 text-white text-lg font-semibold hover:scale-110 active:duration-75 active:scale-95 sm:w-max focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2"
           >
             로그인
           </a>
         </div>
       </div>
-      <p class="mt-4 text-center text-sm text-gray-500">
-        이 처음이신가요?
-        <a
-          href="#"
-          class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
-        >
-          {" "}
-          튜토리얼 보기
-        </a>
+      <p class="group flex inline-flex justify-center mt-4 text-center text-sm text-gray-500">
+        <div>
+          <img
+            src={logo}
+            width="59"
+            height="15"
+            className="group flex inline-flex justify-center overflow-hidden "
+          />
+          이 처음이신가요? &ensp;
+          <a
+            href="/tutorial"
+            class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+            // className="group flex inline-flex justify-center"
+          >
+            {" "}
+            튜토리얼 보기
+          </a>
+        </div>
       </p>
     </div>
   );
