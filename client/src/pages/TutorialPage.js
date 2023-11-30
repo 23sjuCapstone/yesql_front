5f54a79268ec98f9599f573eb874dbe471867a34
sungyeon <csy357@gmail.com>
2023-11-30 12:15:38 +0900

    Style: 튜토리얼 페이지 수정

--- client/src/pages/TutorialPage.js
+++ client/src/pages/TutorialPage.js
@@ -4,8 +4,16 @@ import "./TutorialPage.css";
 
 function TutorialPage() {
   return (
-    <body className="tutorialBody">
-      <img src={tutorital} className="torutialImg"></img>
+    <body className="tutorialBody relative ">
+      <div>
+        <img src={tutorital} className="torutialImg"></img>
+      </div>
+      <a
+        href="/login"
+        className="startButton flex inline-flex w-400 bg-yesql items-center justify-center py-2 px-4 overflow-hidden rounded-full transition-all duration-300 text-white text-lg font-semibold sm:w-max focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2"
+      >
+        시작하기
+      </a>
     </body>
   );
 }
